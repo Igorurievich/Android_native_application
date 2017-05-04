510ba789cb6626df5fbcc1620b1b632af59764dc
SIIPOLSKA\ibodia <ibodia@pl.sii.eu>
2017-05-04 17:24:33 +0200

    Start work with file compressing test.

--- Bodia_benchmark/app/src/main/java/com/example/ibodia/bodia_benchmark/ConvertVideoClipTestActivity.java
+++ Bodia_benchmark/app/src/main/java/com/example/ibodia/bodia_benchmark/ConvertVideoClipTestActivity.java
@@ -2,14 +2,25 @@ package com.example.ibodia.bodia_benchmark;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.io.File;
 
 /**
  * Created by ibodia on 5/1/2017.
  */
 
-public class ConvertVideoClipTestActivity extends AppCompatActivity {
+public class ConvertVideoClipTestActivity extends AppCompatActivity implements View.OnClickListener  {
     public void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_convert_video_clip_test);
+    }
+
+    public void onClick(View view) {
+        String dirPath = getFilesDir().getAbsolutePath() + File.separator + "newfoldername";
+        File projDir = new File(dirPath);
+        if (!projDir.exists()) {
+            projDir.mkdirs();
+        }
     }
 }
