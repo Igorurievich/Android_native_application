27103882a4024a630a51baaca74f2de6a3bb8f7e
Igorurievich <igorurevich94@gmail.com>
2017-05-30 00:37:18 +0200

    Worked version of FPS rate test.

--- Bodia_benchmark/app/src/main/java/com/example/ibodia/bodia_benchmark/FrameRateCameraTestActivity.java
+++ Bodia_benchmark/app/src/main/java/com/example/ibodia/bodia_benchmark/FrameRateCameraTestActivity.java
@@ -1,45 +1,134 @@
 package com.example.ibodia.bodia_benchmark;
 
-import android.content.Intent;
-import android.graphics.Camera;
-import android.net.Uri;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.VideoView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 /**
  * Created by ibodia on 5/29/2017.
  */
 
-public class FrameRateCameraTestActivity extends AppCompatActivity {
-    static final int REQUEST_VIDEO_CAPTURE = 1;
-    private VideoView mVideoView;
+public class FrameRateCameraTestActivity extends AppCompatActivity implements SurfaceHolder.Callback{
+
+    Camera camera;
+    SurfaceView camView;
+    SurfaceHolder surfaceHolder;
+    boolean camCondition = false;
+
+    TextView fpsRate;
+    List<Double> list;
+    Timer timer;
+    double fps;
+    boolean isRecord;
+    TextView fpsAverageValue;
+    TextView fpsMinMaxValue;
+
+    int frames = 0;
+    int avgFPS = 0;
     public void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_fps_camera_test);
+
+        fpsRate = (TextView)findViewById(R.id.fps_rate);
+
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+        camView = (SurfaceView) findViewById(R.id.cameraPreview);
+        surfaceHolder = camView.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(surfaceHolder.SURFACE_TYPE_NORMAL);
+        list = new ArrayList<Double>();
+
+        fpsAverageValue = (TextView) findViewById(R.id.average_fps_value);
+        fpsMinMaxValue = (TextView) findViewById(R.id.min_max_fps_values);
+
+        timer = new Timer();
     }
 
     public void onClick(View view) {
-        dispatchTakeVideoIntent();
+        camera.setPreviewCallback(callback);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                isRecord = false;
+                camera.setPreviewCallback(null);
+                int value = avgFPS/frames;
+                fpsAverageValue.setText(Integer.toString(value));
+            }
+        }, 15000);
     }
 
-        private void dispatchTakeVideoIntent() {
-        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+    private final int MAX_SIZE = 100;
+    private final double NANOS = 1000000000.0;
+
+    LinkedList<Long> times = new LinkedList<Long>(){{
+        add(System.nanoTime());
+    }};
+    private double fps() {
+        long lastTime = System.nanoTime();
+        double difference = (lastTime - times.getFirst()) / NANOS;
+        times.addLast(lastTime);
+        int size = times.size();
+        if (size > MAX_SIZE) {
+            times.removeFirst();
         }
+        return difference > 0 ? times.size() / difference : 0.0;
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
-            Uri videoUri = intent.getData();
-            mVideoView.setVideoURI(videoUri);
+    public void surfaceCreated(SurfaceHolder holder) {
+        camera = Camera.open();
+        camera.setDisplayOrientation(90);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        if(camCondition){
+            camera.stopPreview();
+            camCondition = false;
+        }
+        if (camera!=null)
+        {
+            try {
+                camera.setPreviewDisplay(surfaceHolder);
+                camera.startPreview();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+
         }
     }
+    public Camera.PreviewCallback callback = new Camera.PreviewCallback() {
+        public void onPreviewFrame(byte[] data, Camera camera) {
+            fps = fps();
+            avgFPS += fps;
+            frames++;
+            //fpsRate.setText(Double.toString(fps));
+        }
+    };
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+        camCondition = false;
+    }
+
 }
