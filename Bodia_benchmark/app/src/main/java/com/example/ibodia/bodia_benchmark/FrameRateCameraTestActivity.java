6dbef86a7f97e757968d5c1d7e18186177727360
Igorurievich <igorurevich94@gmail.com>
2017-05-30 14:58:33 +0200

    Changes in video tests. Stable version. Check recorded video.

--- Bodia_benchmark/app/src/main/java/com/example/ibodia/bodia_benchmark/FrameRateCameraTestActivity.java
+++ Bodia_benchmark/app/src/main/java/com/example/ibodia/bodia_benchmark/FrameRateCameraTestActivity.java
@@ -1,134 +1,74 @@
 package com.example.ibodia.bodia_benchmark;
 
-import android.graphics.PixelFormat;
-import android.hardware.Camera;
+import android.content.Intent;
+import android.media.MediaExtractor;
+import android.media.MediaFormat;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-
 
 /**
  * Created by ibodia on 5/29/2017.
  */
 
-public class FrameRateCameraTestActivity extends AppCompatActivity implements SurfaceHolder.Callback{
-
-    Camera camera;
-    SurfaceView camView;
-    SurfaceHolder surfaceHolder;
-    boolean camCondition = false;
+public class FrameRateCameraTestActivity extends AppCompatActivity{
 
     TextView fpsRate;
-    List<Double> list;
-    Timer timer;
-    double fps;
-    boolean isRecord;
-    TextView fpsAverageValue;
-    TextView fpsMinMaxValue;
 
-    int frames = 0;
-    int avgFPS = 0;
     public void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_fps_camera_test);
-
-        fpsRate = (TextView)findViewById(R.id.fps_rate);
-
-        getWindow().setFormat(PixelFormat.UNKNOWN);
-        camView = (SurfaceView) findViewById(R.id.cameraPreview);
-        surfaceHolder = camView.getHolder();
-        surfaceHolder.addCallback(this);
-        surfaceHolder.setType(surfaceHolder.SURFACE_TYPE_NORMAL);
-        list = new ArrayList<Double>();
-
-        fpsAverageValue = (TextView) findViewById(R.id.average_fps_value);
-        fpsMinMaxValue = (TextView) findViewById(R.id.min_max_fps_values);
-
-        timer = new Timer();
+        fpsRate = (TextView) findViewById(R.id.fps_value);
     }
-
     public void onClick(View view) {
-        camera.setPreviewCallback(callback);
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                isRecord = false;
-                camera.setPreviewCallback(null);
-                int value = avgFPS/frames;
-                fpsAverageValue.setText(Integer.toString(value));
-            }
-        }, 15000);
+        dispatchTakeVideoIntent();
     }
 
-    private final int MAX_SIZE = 100;
-    private final double NANOS = 1000000000.0;
+    static final int REQUEST_VIDEO_CAPTURE = 1;
 
-    LinkedList<Long> times = new LinkedList<Long>(){{
-        add(System.nanoTime());
-    }};
-    private double fps() {
-        long lastTime = System.nanoTime();
-        double difference = (lastTime - times.getFirst()) / NANOS;
-        times.addLast(lastTime);
-        int size = times.size();
-        if (size > MAX_SIZE) {
-            times.removeFirst();
+    private void dispatchTakeVideoIntent() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
         }
-        return difference > 0 ? times.size() / difference : 0.0;
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        camera = Camera.open();
-        camera.setDisplayOrientation(90);
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        if(camCondition){
-            camera.stopPreview();
-            camCondition = false;
-        }
-        if (camera!=null)
-        {
-            try {
-                camera.setPreviewDisplay(surfaceHolder);
-                camera.startPreview();
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = intent.getData();
+            DoSmth(videoUri);
         }
     }
-    public Camera.PreviewCallback callback = new Camera.PreviewCallback() {
-        public void onPreviewFrame(byte[] data, Camera camera) {
-            fps = fps();
-            avgFPS += fps;
-            frames++;
-            //fpsRate.setText(Double.toString(fps));
-        }
-    };
 
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        camera.stopPreview();
-        camera.release();
-        camera = null;
-        camCondition = false;
-    }
+   private void DoSmth(Uri uriVideo)
+   {
+       MediaExtractor extractor = new MediaExtractor();
+       int frameRate = 24;
+       try {
+           extractor.setDataSource(uriVideo.getPath());
+           int numTracks = extractor.getTrackCount();
+           for (int i = 0; i < numTracks; ++i) {
+               MediaFormat format = extractor.getTrackFormat(i);
+               String mime = format.getString(MediaFormat.KEY_MIME);
+               if (mime.startsWith("video/")) {
+                   if (format.containsKey(MediaFormat.KEY_FRAME_RATE)) {
+                       frameRate = format.getInteger(MediaFormat.KEY_FRAME_RATE);
+                       fpsRate.setText(Integer.toString(frameRate));
+                   }
+               }
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }finally {
+           extractor.release();
+       }
+   }
 
 }
